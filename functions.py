ae3c56a8c1c56282971dbbe175c300b1d94e5c24
Soumil <soumilgad@gmail.com>
2023-03-10 21:06:37 -0800

    complete timeDifference function

--- functions.py
+++ functions.py
@@ -1,24 +1,25 @@
 import pandas as pd
-import datetime as dt
+from datetime import datetime, date
 import re
 
 def convertTime(time):
-    if time is None or pd.isnull(time):
-        return None
-
     form = re.match(r'(\d{1,2}):(\d{2})(\s*[AP]M)', time)
     if form:
         try:
-            time = pd.to_datetime(time, format='%I:%M%p')
+            time = date.strptime(time, '%H:%M%p')
         except ValueError:
-            time = pd.to_datetime(time, format='%I:%M %p')
+            time = date.strptime(time, '%H:%M %p')
 
     else:
         try:
-            time = pd.to_datetime(time, format='%Y-%m-%d %I:%M%p')
+            time = date.strptime(time, '%Y-%m-%d %H:%M%p')
         except ValueError:
-            time = pd.to_datetime(time, format='%Y-%m-%d %I:%M %p')
+            time = date.strptime(time, '%Y-%m-%d %H:%M %p')
     
     # Return time as a datetime object
-    time = time.time()
     return time
+
+def timeDifference(time1, time2):
+    res = datetime.combine(date.min, time2) - datetime.combine(date.min, time1)
+    res = int(res.total_seconds() / 60)
+    return res
